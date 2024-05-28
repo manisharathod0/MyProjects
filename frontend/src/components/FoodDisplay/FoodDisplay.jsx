42524c6928d9e9f3caf4d74fb5fe2709916ef57c
Manisha Rathod <manisharathod0609@gmail.com>
2024-05-28 13:45:08 +0530

    commit

---
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,57 @@
+// import React, { useContext } from 'react'
+// import './FoodDisplay.css'
+// import { StoreContext } from '../../context/StoreContext'
+// import FoodItem from '../FoodItem/FoodItem'
+
+// const FoodDisplay = (category) => {
+
+//     const {food_list} = useContext(StoreContext)
+
+//   return (
+    
+//     <div className='food-display' id='food-display'>
+//         <h2>Top dishes near you</h2>
+//         <div className="food-display-list">
+//             {food_list.map((item,index)=>{
+//               {console.log(category,item.category);}
+//               if (category==="All" || category===item.category){
+//                 return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+//               }
+//             })}
+//         </div>
+//     </div>
+
+  
+//   )
+// }
+
+// export default FoodDisplay
+
+
+
+import React, { useContext } from 'react'
+import './FoodDisplay.css'
+import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
+
+const FoodDisplay = (props) => { // Accept props here
+
+    const { food_list } = useContext(StoreContext)
+
+    return (
+        <div className='food-display' id='food-display'>
+            <h2>Top dishes near you</h2>
+            <div className="food-display-list">
+                {food_list.map((item, index) => {
+                    console.log(props.category, item.category);
+                    if (props.category === "All" || props.category === item.category) {
+                        return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+                    }
+                    return null; // Return null when the condition is not met
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default FoodDisplay
